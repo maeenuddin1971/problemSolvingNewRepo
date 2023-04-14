625bc86a19d451f68944bcba05fa418c956ca2ea
maeen <maeenuddin1971@gmail.com>
2023-04-14 20:22:35 +0600

    Hackerrank Min-Max sum solution

---
+++ src/hackerrank/java/MinMaxSum.java
@@ -0,0 +1,36 @@
+package hackerrank.java;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinMaxSum {
+
+    public static void main(String[] args) {
+        Scanner r = new Scanner(System.in);
+        Long[] p = new Long[5];
+
+        for (int i = 0; i < 5; i++) {
+            p[i] = r.nextLong();
+        }
+
+        Arrays.sort(p);
+
+
+        Long minSum = Long.valueOf(0);
+        Long  maxSum = Long.valueOf(0);
+
+        for (int i = 0; i < 5; i++) {
+            if (i != 4) {
+                minSum = minSum + p[i];
+            }
+
+            if (i != 0) {
+                maxSum = maxSum + p[i];
+            }
+        }
+
+        System.out.println(minSum + " " + maxSum);
+
+    }
+
+}
