1b5767f27587abd5ee602ac1b4c02aff0fb4418f
maeen <maeenuddin1971@gmail.com>
2023-06-15 19:51:21 +0600

    Hackerrank Mock test codes are pushed

---
+++ src/hackerrank/java/FindTheMedian.java
@@ -0,0 +1,18 @@
+package hackerrank.java;
+
+import java.util.*;
+
+public class FindTheMedian {
+    public static void main(String[] args) {
+        Scanner r= new Scanner(System.in);
+        int t=r.nextInt();
+        List<Integer> arr = new ArrayList<>();
+
+        for(int i =0;i<t;i++) {
+            arr.add(r.nextInt());
+        }
+
+        Collections.sort(arr);
+        System.out.println(arr.get((arr.size()-1)/2));
+    }
+}
