f22ff4efdf673805926afcd0b2f6db0c015b605e
maeen <maeenuddin1971@gmail.com>
2024-12-15 22:51:37 +0800

    New problem fixed

---
+++ src/hackerrank/java/month/week2/CounterGame.java
@@ -0,0 +1,35 @@
+package hackerrank.java.month.week2;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class CounterGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int totalNumber = scanner.nextInt();
+
+        for (int i = 0; i < totalNumber; i++) {
+            Long bigInteger = scanner.nextLong();
+            int step = 0;
+
+            //System.out.println(step);
+            System.out.println(counterGame(bigInteger));
+        }
+    }
+
+    public static String counterGame(long n)
+    {
+        int count = 0;
+        long a;
+        while (n > 1) {
+            a = (long)Math.pow(2, Long.toBinaryString(n).length() - 1);
+            n = n == a ? n - a : n / 2;
+            count++;
+        }
+
+        return count % 2 == 0 ? "Richard" : "Louise";
+    }
+
+
+}
---
+++ src/hackerrank/java/month/week2/NewYearChaos.java
@@ -0,0 +1,64 @@
+package hackerrank.java.month.week2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class NewYearChaos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int ph = scanner.nextInt();
+
+        for (int ip = 0; ip < ph; ip++) {
+
+            int n = scanner.nextInt();
+
+            int[] p = new int[n];
+            Map<Integer, Integer> dataMap = new HashMap<>();
+            Map<Integer, Integer> dataPosition = new HashMap<>();
+
+            for (int i = 0; i < n; i++) {
+                p[i] = scanner.nextInt();
+                dataMap.put(p[i], i + 1);
+                dataPosition.put(i + 1, p[i]);
+            }
+
+
+            Boolean isChaotic = Boolean.FALSE;
+            int sum = 0;
+            for (int u = n; u >= 1; u--) {
+                int position = u - dataMap.get(u);
+
+                if (position == 0) {
+                    continue;
+                }
+
+                if (position > 2) {
+                    isChaotic = Boolean.TRUE;
+                    break;
+                } else {
+                    if (position == 1) {
+                        dataMap.put(dataPosition.get(u), u - 1);
+                        dataPosition.put(u - 1, dataPosition.get(u));
+                    } else if (position == 2) {
+                        int secondData = dataPosition.get(u - 1);
+                        dataMap.put(dataPosition.get(u), u - 1);
+                        dataPosition.put(u - 1, dataPosition.get(u));
+                        dataMap.put(secondData, u - 2);
+                        dataPosition.put(u - 2, secondData);
+                    }
+                    sum = sum + position;
+                    dataMap.put(u, u);
+                    dataPosition.put(u, u);
+                }
+            }
+
+            if (isChaotic) {
+                System.out.println("Too chaotic");
+            } else {
+                System.out.println(sum);
+            }
+        }
+    }
+}
